a5f4d04a02730c1aee6997a4370d2a8ca27d9c6a
Collin Pham <collinpham2018@u.northwestern.edu>
2017-10-31 16:44:14 -0500

    adding PIL API to server

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var search = require('./lib/search/search.js')
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// Handle all GET requests
+app.get('*', (req, res) => {
+  res.type('text/html'); 
+  res.status(200); 
+  res.send('Terminal 49 API')
 });
+
+// Main API functionality
+app.post('/search', (req, res) => {
+  // Parse request body
+  const bookingNumber = req.body.bookingNumber
+  const steamshipLine = req.body.steamshipLine
+
+  // Error Check
+  if (!bookingNumber) { res.status(400).send('Missing required field bookingNumber') }
+  if (!steamshipLine) { res.status(400).send('Missing required field steamshipLine') }
+
+  // Search for booking details based on a given bookingNumber and steamshipLine
+  search.byBookingNumberAndSteamshipLine(bookingNumber, steamshipLine)
+  .then((data) => { res.send(data) })
+  .catch((err) => { res.status(400).send(err) })
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
